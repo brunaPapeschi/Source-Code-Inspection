9ea12b0e6f83d13ce2c8a68898eaac4c490d8d87
rafaela.santos <>
2017-11-13 10:35:57 -0200

    Teste primeiro commit

--- Source Code Inspection/src/br/calebe/ticketmachine/exception/PapelMoedaInvalidaException.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/exception/PapelMoedaInvalidaException.java
@@ -1,9 +1,12 @@
 package br.calebe.ticketmachine.exception;
 
+
 /**
  *
  * @author Calebe de Paula Bianchini
  */
 public class PapelMoedaInvalidaException extends Exception {
-    
+    public static void main(String[] args) {
+        System.out.println("Testando o GitHub");
+    }
 }
